4c43aa81fb1caaf1ba07cec0720be82f4659d184
Kazetama <tamaketuahimpunan@gmail.com>
2026-02-24 08:27:34 +0700

    chore: clean up old project files

--- resources/js/components/profile/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as StudentForm } from './student-form'
-export { default as LecturerForm } from './lecturer-form'
--- resources/js/types/lecturer.ts
+++
@@ -1,5 +0,0 @@
-export interface Lecturer {
-    nidn?: string
-    spesialisasi?: string
-    jabatan_fungsional?: string
-}
